42c0252cbd1b5d5c34ccbc39e1c6afe69a41b73e
Matt Pickle <matthew.e.pickle@gmail.com>
2025-07-23 17:39:38 -0400

    refactor script for readability
    decided there's no reason I need to be messing with the ComicInfo.xml files as I can tell what's a oneshot or not just from the SERIES tbale.

--- korrector/korrector.py
+++ korrector/korrector.py
@@ -1,14 +1,16 @@
 """Class for accumulated information about an issue from different sources"""
-import glob
-import os
 import re
 import shutil
 import sqlite3
-import tempfile
-import zipfile
 from datetime import datetime
+from typing import TypedDict
 
-import untangle
+
+class Series(TypedDict):
+    id: str
+    name: str
+    year: str
+    oneshot: bool
 
 
 def format_sql(sql_cmd: str) -> str:
@@ -25,146 +27,11 @@ def format_sql(sql_cmd: str) -> str:
     return " ".join(sql_cmd.split())
 
 
-def update_oneshots(oneshot_path: str):
-    """Update one-shots from a given directory.
-
-    Iterate through all cbz files in the given directory and its subdirectories.
-    For each cbz file, extract the "ComicInfo.xml" file and copy the "Series" field
-    to the "Title" field, or create a new "Title" field if it doesn't exist.
-
-    Args:
-        oneshot_path: The path to the directory containing the one-shot cbz files.
-    Returns:
-        None
-    """
-    # Iterate recursively through One-Shots directory (see https://komga.org/docs/guides/oneshots/)
-    for cbz_file in glob.iglob(oneshot_path + "/**/*.cbz", recursive=True):
-        # get komga.series_title (story_title) and metron.series_id from ComicInfo.xml
-        with zipfile.ZipFile(cbz_file, "r") as zip_:
-            try:
-                zip_.extract("ComicInfo.xml")
-            except KeyError:
-                continue
-        untangled = untangle.parse("ComicInfo.xml").ComicInfo
-        if "MetronTagger" in untangled.Notes.cdata:
-            try:
-                with open(
-                        "ComicInfo.xml", "r", encoding="utf-8", errors="replace"
-                ) as f:
-                    contents = f.read()
-                    encode = "utf-8"
-            except UnicodeDecodeError:
-                with open(
-                        "ComicInfo.xml", "r", encoding="latin-1", errors="replace"
-                ) as f:
-                    contents = f.read()
-                    encode = "latin-1"
-            story_title = ""
-            series_name = ""
-            for line in contents.split("\n"):
-                if "<Series>" in line:
-                    series_name = line.split(">")[1].split("<")[0]
-                if "<Title>" in line:
-                    story_title = line.split(">")[1].split("<")[0]
-            if story_title == series_name:
-                continue
-            print(f"Updating {cbz_file}")
-            # write metron.series_title to Title field in ComicInfo.xml
-            if story_title and series_name:
-                contents = contents.replace(story_title, series_name)
-            # or add title field with metron.series_title
-            if series_name and not story_title:
-                series_tag = f"<Series>{series_name}</Series>"
-                i = contents.index(series_tag)
-                contents = contents[:i] + f"<Title>{series_name}</Title>" + contents[i:]
-            # replace old ComicInfo.xml with new one
-            with open("ComicInfo.xml", "w", encoding=encode) as f:
-                f.write(contents)
-            tmpfd, tmpname = tempfile.mkstemp(dir=os.path.dirname(cbz_file))
-            os.close(tmpfd)
-            # rebuild cbz
-            info_name = "ComicInfo.xml"
-            with zipfile.ZipFile(cbz_file, "r") as zipin:
-                with zipfile.ZipFile(tmpname, "w") as zipout:
-                    zipout.comment = zipin.comment
-                    for item in zipin.infolist():
-                        if item.filename != info_name:
-                            zipout.writestr(item, zipin.read(item.filename))
-            # replace old zip with new one
-            os.remove(cbz_file)
-            os.rename(tmpname, cbz_file)
-            # add new contents
-            with zipfile.ZipFile(
-                    cbz_file, "a", compression=zipfile.ZIP_DEFLATED
-            ) as zip_:
-                zip_.writestr("ComicInfo.xml", contents)
-            print("done.")
-        # remove old contents
-        os.remove("ComicInfo.xml")
-
-
-def update_komga_series(komga_config_path: str) -> None:
-    """update the komga database's series metadata to be compatible with DieselTech reading lists
-
-    When you try to add a reading list to komga, it will check the 'title' field in the
-    'SERIES_METADATA table for a name of the format 'series_name (year_began)'. But, for whatever
-    reason, this is not how komga initially stores the series metadata. This function updates the
-    'title' field so that komga will properly match issues with when given a DieselTech reading
-    list. Will not work with series located in the oneshots folder as Komga uses the
-    COMIC_METADATA table to match reading lists for oneshots.
-
-    Args:
-        komga_config_path: path to komga database
-    """
-
-    con = sqlite3.connect(f"{komga_config_path}\\database.sqlite")
+def korrect(komga_db_path: str, komga_backup: str) -> None:
+    backup(komga_db_path, komga_backup)
+    con = sqlite3.connect(f"{komga_db_path}")
     cur = con.cursor()
-    # store all series ids in series_id
-    sql_cmd = format_sql(
-        """
-        SELECT id
-        FROM series
-        """
-    )
-    series_ids = cur.execute(sql_cmd).fetchall()
-    series = []
-    # map series names to ids
-    for id_ in series_ids:
-        id_ = id_[0]
-        sql_cmd = format_sql(
-            f'''
-            SELECT name
-            FROM series
-            WHERE id is "{id_}"
-            '''
-        )
-        series_name = cur.execute(sql_cmd).fetchone()[0]
-        series.append({"id": id_, "name": series_name})
-    # get start years for each series
-    for s in series:
-        # get every book from each series
-        sql_cmd = format_sql(
-            f'''
-            SELECT id
-            FROM book
-            WHERE series_id is "{s["id"]}"
-            '''
-        )
-        book_ids = cur.execute(sql_cmd).fetchall()
-        release_years = []
-        for book_id in book_ids:
-            sql_cmd = format_sql(
-                f'''
-                SELECT release_date
-                FROM book_metadata
-                WHERE book_id is "{book_id[0]}"
-                '''
-            )
-            release_date = cur.execute(sql_cmd).fetchone()[0]
-            if release_date:
-                release_years.append(int(release_date[:4]))
-        s["year"] = min(release_years)
-    # update titles
+    series = get_series(cur)
     for s in series:
         sql_cmd = format_sql(
             f'''
@@ -188,101 +55,80 @@ def update_komga_series(komga_config_path: str) -> None:
         )
         cur.execute(sql_cmd)
     con.commit()
-    print("done.")
 
 
-def update_komga_series_oneshots(oneshot_path: str, komga_config_path: str) -> None:
-    """Update the title field in the Komga database for oneshot books.
-
-    This function updates the title of each oneshot book in the Komga database
-    to include the release year in the format 'title (year)'. It retrieves the
-    list of CBZ files from the specified oneshot directory, constructs URLs
-    for each file, and matches them to the corresponding book entries in the
-    Komga database. If the title does not already include the year, it appends
-    the release year to the title.
+def backup(komga_db_path: str, komga_backup: str) -> None:
+    """Backup the Komga database to a specified backup path.
 
     Args:
-        oneshot_path (str): Path to the directory containing oneshot CBZ files.
-        komga_config_path (str): Path to the Komga database configuration.
+        komga_db_path (str): The path to the Komga database.
+        komga_backup (str): The path where the backup should be stored.
+    """
+    backup_name = datetime.now().strftime("%Y-%m-%d(%H_%M_%S)")
+    src = f"{komga_db_path}"
+    dest = f"{komga_backup}\\{backup_name}\\"
+    shutil.copytree(src, dest)
+
+
+def get_series(cur: sqlite3.Cursor) -> list[Series]:
+    """Retrieve all series from the database with their id, name, and start year.
+
+    This function queries the `series` table to get all series ids and names,
+    then determines the earliest release year for each series by examining the
+    `release_date` field in the `book_metadata` table for all books in the series.
+    If there isn't a first issue for a series, it will skip that series and log a warning.
+
+    Args:
+        cur (sqlite3.Cursor): The database cursor to use for queries.
 
     Returns:
-        None
+        list[Series]: A list of dictionaries, each containing the id, name, year, and oneshot of a series.
     """
-
-    con = sqlite3.connect(f"{komga_config_path}\\database.sqlite")
-    cur = con.cursor()
-    files = [
-        f"file:/data{y}"
-        for x in os.walk(oneshot_path)
-        for y in glob.glob(os.path.join(x[0], "*.cbz"))
-    ]
-    urls = []
-    for file in files:
-        url = re.sub(r"Z:\\print", r"", file)
-        url = re.sub(r"\\", r"/", url)
-        url = re.sub(r"print/", r"", url)
-        url = re.sub(r" ", r"%20", url)
-        url = re.sub(r"#", r"%23", url)
-        url = re.sub(r"ñ", r"%C3%B1", url)
-
-        urls.append(url)
-    ids = []
-    for url in urls:
+    # store all series ids in series_id
+    sql_cmd = format_sql(
+        """
+        SELECT id
+        FROM series
+        """
+    )
+    series_ids = cur.execute(sql_cmd).fetchall()
+    series = []
+    # map series names to ids
+    for id_ in series_ids:
+        id_ = id_[0]
+        sql_cmd = format_sql(
+            f'''
+            SELECT name
+            FROM series
+            WHERE id is "{id_}"
+            '''
+        )
+        series_name = cur.execute(sql_cmd).fetchone()[0]
+        series.append({"id": id_, "name": series_name})
+    # TODO: if a book doesn't have a first issue, the function should skip it and log a warning
+    # get start years for each series
+    for s in series:
+        # get every book from each series
         sql_cmd = format_sql(
             f'''
             SELECT id
-            FROM  BOOK
-            WHERE url is "{url}"
+            FROM book
+            WHERE series_id is "{s["id"]}"
             '''
         )
-        _id = cur.execute(sql_cmd).fetchone()[0]
-        ids.append(_id)
-    for id_ in ids:
-        sql_cmd = format_sql(
-            f'''
-            SELECT title
-            FROM book_metadata 
-            WHERE book_id is "{id_}"
-            '''
-        )
-        old_title = cur.execute(sql_cmd).fetchone()[0]
-        if "(" in old_title and ")" in old_title:
-            continue
-        sql_cmd = format_sql(
-            f'''
-            SELECT release_date
-            FROM book_metadata
-            WHERE book_id is "{id_}"
-            '''
-        )
-        year = int(cur.execute(sql_cmd).fetchone()[0][:4])
-        new_title = f"{old_title} ({year})"
-        sql_cmd = format_sql(
-            f'''
-            UPDATE book_metadata
-            SET title = "{new_title}"
-            WHERE book_id is "{id_}"
-            '''
-        )
-        cur.execute(sql_cmd)
-    con.commit()
-
-
-def update_komga(
-        oneshot_path: str, komga_config_path: str, komga_backup_path: str
-) -> None:
-    """Backs up the current Komga db and updates the series titles
-
-    Args:
-        oneshot_path (str): The path to the oneshots folder.
-        komga_config_path (str): The path to the Komga configuration folder.
-        komga_backup_path (str):
-            The path to the folder where backups of the Komga configuration should be stored.
-    """
-    update_oneshots(oneshot_path)
-    backup_name = datetime.now().strftime("%Y-%m-%d(%H_%M_%S)")
-    src = f"{komga_config_path}\\"
-    dest = f"{komga_backup_path}\\{backup_name}\\"
-    shutil.copytree(src, dest)
-    update_komga_series(komga_config_path)
-    # update_komga_series_oneshots(oneshot_path, komga_config_path)
+        book_ids = cur.execute(sql_cmd).fetchall()
+        # get release year for series
+        release_years = []
+        for book_id in book_ids:
+            sql_cmd = format_sql(
+                f'''
+                SELECT release_date
+                FROM book_metadata
+                WHERE book_id is "{book_id[0]}"
+                '''
+            )
+            release_date = cur.execute(sql_cmd).fetchone()[0]
+            if release_date:
+                release_years.append(int(release_date[:4]))
+        s["year"] = min(release_years)
+    return series
