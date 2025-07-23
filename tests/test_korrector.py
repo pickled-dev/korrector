dbe3be5c42482b9603ab27941eff45f4a850f75e
Matt Pickle <matthew.e.pickle@gmail.com>
2025-07-23 13:08:42 -0400

    initial commit

---
+++ tests/test_korrector.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from korrector.korrector import update_komga
+
+
+class TestKorrector(TestCase):
+
+    def test_update_komga(self):
+        self.fail()
+
+    def test_update_oneshots(self):
+        self.fail()
+
+    def test_update_komga_series(self):
+        self.fail()
+
+    def test_update_komga_series_oneshots(self):
+        self.fail()
