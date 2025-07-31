f620836d85d3aafd76af39705eb600b50165cf72
Matt Pickle <matthew.e.pickle@gmail.com>
2025-07-31 17:49:40 -0400

    fixes import issue with copy_share_to_sort

--- korrector/__init__.py
+++ korrector/__init__.py
@@ -1,5 +1,6 @@
 """Korrector package: tools for Komga database correction."""
 
+from .main import copy_share_to_sort as copy_share_to_sort
 from .main import korrect_comic_info_path as korrect_comic_info_path
 from .main import korrect_database as korrect_database
 from .main import korrect_database_oneshots as korrect_database_oneshots
